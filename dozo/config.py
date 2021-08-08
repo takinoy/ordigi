308c1eef5c6a2248c3468aca4b246ec79cfa213f
Cedric Leporcq <takino@hadoly.fr>
2021-08-08 12:40:48 +0200

    Drop plugin support

--- dozo/config.py
+++ dozo/config.py
@@ -15,26 +15,6 @@ def load_config(file):
     load_config.config.read(file)
     return load_config.config
 
-def load_plugin_config(file):
-    config = load_config(file)
-
-    # If plugins are defined in the config we return them as a list
-    # Else we return an empty list
-    if 'Plugins' in config and 'plugins' in config['Plugins']:
-        return config['Plugins']['plugins'].split(',')
-
-    return []
-
-def load_config_for_plugin(name, file):
-    # Plugins store data using Plugin%PluginName% format.
-    key = 'Plugin{}'.format(name)
-    config = load_config(file)
-
-    if key in config:
-        return config[key]
-
-    return {}
-
 
 def get_path_definition(config):
     """Returns a list of folder definitions.
